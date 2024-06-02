0a39a260ff9d872a94ea92bffd9839fa7e059af0
Pranto Deb <rajprit0m@outlook.com>
2024-06-03 02:43:14 +0600

    install stripe and payment route created and user register route created

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 const accessToken = process.env.ACCESS_TOKEN;
+const stripeSecret = process.env.STRIPE_SECRET_KEY;
+const stripe = require("stripe")(stripeSecret);
 
 app.use(cors());
 app.use(express.json());
@@ -25,17 +27,52 @@ const client = new MongoClient(url, {
 async function run() {
   try {
     await client.connect();
+    const db = client.db("smartCom");
+    const usersCollection = db.collection("users");
 
+    /******************************* Payment ******************************************/
+    // payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const price = req.body.price;
+      const priceInCent = parseFloat(price) * 100;
+      if (!price || priceInCent < 1) return;
+      const { client_secret } = await stripe.paymentIntents.create({
+        amount: priceInCent,
+        currency: "usd",
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      });
+      res.send({ clientSecret: client_secret });
+    });
+    /******************************* Payment ******************************************/
 
+    /******************************* Users ******************************************/
+    // get role
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      let role = '';
+      if (user) {
+        role = user?.role;
+      }
+      res.send({ role: role });
+    });
 
-
-
+    // create user
+    app.post("/add-hr", async (req, res) => {
+      const data = req.body;
+      const addedRole = { ...data, role: "HR" };
+      const result = await usersCollection.insertOne(addedRole);
+      res.send(result);
+    });
+    // update user
+    /******************************* Payment ******************************************/
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    
     // await client.close();
   }
 }
